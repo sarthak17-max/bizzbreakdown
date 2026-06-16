e7cc57040d3b07d75c8e9de7e109b98982c1c12e
sarthak gajanan mahalle <sarthakgajananmahalle@sarthaks-MacBook-Air.local>
2026-06-16 10:07:06 +0530

    Add bottom nav to compare, industries, saved pages

--- app/industries/page.tsx
+++ app/industries/page.tsx
@@ -1,12 +1,12 @@
-import SavedHeader from "../../src/components/SavedHeader";
-import SavedEmptyState from "../../src/components/SavedEmptyState";
+import IndustriesHeader from "../../src/components/IndustriesHeader";
+import IndustriesList from "../../src/components/IndustriesList";
 import BottomNav from "../../src/components/BottomNav";
 
-export default function SavedPage() {
+export default function IndustriesPage() {
   return (
     <main className="min-h-screen bg-white w-full max-w-5xl mx-auto pb-20">
-      <SavedHeader />
-      <SavedEmptyState />
+      <IndustriesHeader />
+      <IndustriesList />
       <BottomNav />
     </main>
   );
--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -1,11 +1,13 @@
 import SavedHeader from "../../src/components/SavedHeader";
 import SavedEmptyState from "../../src/components/SavedEmptyState";
+import BottomNav from "../../src/components/BottomNav";
 
 export default function SavedPage() {
   return (
-  <main className="min-h-screen bg-white w-full max-w-5xl mx-auto pb-20">
+    <main className="min-h-screen bg-white w-full max-w-5xl mx-auto pb-20">
       <SavedHeader />
       <SavedEmptyState />
+      <BottomNav />
     </main>
   );
 }
